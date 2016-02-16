e2c58e6c41b4893463824be1d7563e78234d2f86
Christian Claus <ch.claus@me.com>
2016-02-16 07:23:13 +0100

    Completed Test for ExecuteAroundPattern.

--- utils-examples/src/test/java/de/chclaus/examples/functional/ea/ExecuteAroundPatternTest.java
+++ utils-examples/src/test/java/de/chclaus/examples/functional/ea/ExecuteAroundPatternTest.java
@@ -1,19 +1,54 @@
 package de.chclaus.examples.functional.ea;
 
+import de.chclaus.examples.OutputStreamCapture;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
+import java.net.URL;
+
 /**
+ * Tests if the classic method {@link ExecuteAroundPattern#handleFile(URL)}, which prints the content of a file
+ * to System.out, does exactly the same as the new method {@link ExecuteAroundPattern#handleFileWithEA(URL)}.
+ *
  * @author Christian Claus (c.claus@micromata.de)
  */
 public class ExecuteAroundPatternTest {
 
-  @Test
-  public void testHandleFile() throws Exception {
-    new ExecuteAroundPattern().handleFile(getClass().getResource("input.txt"));
+  @Rule
+  public OutputStreamCapture capture = new OutputStreamCapture();
+
+  private URL resource;
+
+  @Before
+  public void setUp() throws Exception {
+    resource = getClass().getResource("input.txt");
   }
 
+  /**
+   * Executes {@link ExecuteAroundPattern#handleFile(URL)} which reads and print a file. Subsequently
+   * there is an assertion for the correct Console-Output.
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testHandleFile() throws Exception {
+    new ExecuteAroundPattern().handleFile(resource);
+    String s = capture.toString();
+    Assert.assertEquals("foo\nbar\n", s);
+  }
+
+  /**
+   * Executes {@link ExecuteAroundPattern#handleFile(URL)} which reads and print a file. Subsequently
+   * there is an assertion for the correct Console-Output.
+   *
+   * @throws Exception
+   */
   @Test
   public void testHandleFileWithEA() throws Exception {
-    new ExecuteAroundPattern().handleFile(getClass().getResource("input.txt"));
+    new ExecuteAroundPattern().handleFile(resource);
+    String s = capture.toString();
+    Assert.assertEquals("foo\nbar\n", s);
   }
 }
